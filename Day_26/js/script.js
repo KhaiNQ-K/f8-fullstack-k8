d786458ddb7bc86a2b4cea248b98fbccbd9c4fa2
KhaiNguyenQ <khainq@atin.vn>
2024-06-25 16:41:16 +0700

    add bài 3

--- Day_26/js/script.js
+++ Day_26/js/script.js
@@ -13,3 +13,87 @@ Array.prototype.filter2 = function (callback) {
   }
   return result;
 };
+// Bài 3
+const app = {
+  categories: [
+    {
+      id: 1,
+      name: 'Chuyên mục 1',
+    },
+    {
+      id: 2,
+      name: 'Chuyên mục 2',
+      children: [
+        {
+          id: 4,
+          name: 'Chuyên mục 2.1',
+        },
+        {
+          id: 5,
+          name: 'Chuyên mục 2.2',
+          children: [
+            {
+              id: 10,
+              name: 'Chuyên mục 2.2.1',
+            },
+            {
+              id: 11,
+              name: 'Chuyên mục 2.2.2',
+            },
+            {
+              id: 12,
+              name: 'Chuyên mục 2.2.3',
+            },
+          ],
+        },
+        {
+          id: 6,
+          name: 'Chuyên mục 2.3',
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Chuyên mục 3',
+      children: [
+        {
+          id: 7,
+          name: 'Chuyên mục 3.1',
+        },
+        {
+          id: 8,
+          name: 'Chuyên mục 3.2',
+        },
+        {
+          id: 9,
+          name: 'Chuyên mục 3.3',
+        },
+      ],
+    },
+  ],
+  result: [],
+  mapData(categories) {},
+  generateOption(category, prefix = '') {
+    return `<option value="${category.id}">${prefix} ${category.name}</option>`;
+  },
+  generateOptions(categories, prefix = '') {
+    return categories
+      .map((category) => {
+        if (category.children && category.children.length > 0) {
+          return (
+            this.generateOption(category, prefix) +
+            this.generateOptions(category.children, prefix + '---|')
+          );
+        } else {
+          return this.generateOption(category, prefix);
+        }
+      })
+      .join('');
+  },
+  run() {
+    this.result = this.generateOptions(this.categories);
+  },
+};
+app.run();
+var select = document.querySelector('#select');
+select.innerHTML = app.result;
